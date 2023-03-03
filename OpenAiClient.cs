b8c622c7a36c256598ac4d10d540d73632227622
Yang Tian <v-yangtian@micosoft.com>
2023-03-03 12:19:52 +0800

    Add project files.

---
+++ OpenAiClient.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatGPT
+{
+    public class OpenAiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public OpenAiClient(string apiKey)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        }
+
+        public async Task<string> SendRequest(string messages, string model)
+        {
+            var requestParams = new
+            {
+                messages = new List<object>() { new { role = "user", content = messages } },
+                model = model,
+            };
+
+            var requestJson = JsonSerializer.Serialize(requestParams);
+            var requestContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+
+            // api documents : https://platform.openai.com/docs/api-reference/chat/create
+
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/chat/completions", requestContent);
+
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using ChatGPT;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        string myDocsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string filePath = Path.Combine(myDocsPath, "conversation.json");
+
+        var client = new OpenAiClient("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");  // generated api key from https://platform.openai.com/account/api-keys
+
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Ask: ");
+
+            string? message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message))
+            {
+                await Console.Out.WriteLineAsync("Ask message must not null or not empty!");
+            }
+            else
+            {
+                try
+                {
+                    var response = await client.SendRequest(message, "gpt-3.5-turbo-0301");
+                    var answer = JObject.Parse(response)?["choices"]?[0]?["message"]?["content"]?.ToString();
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Answer:{answer}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
+
